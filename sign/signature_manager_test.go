e281bbcf78503b33e26bf69dea9980b9d17e7fee
Flajt <matrimkurz@gmail.com>
2023-11-20 14:44:07 +0100

    Fix: Replace NewScaleWayWrapperForDev with NewMockIScaleWayWrapper

--- sign/signature_manager_test.go
+++ sign/signature_manager_test.go
@@ -9,20 +9,21 @@ import (
 	"testing"
 
 	scw_secret_wrapper "github.com/Flajt/decentproof-backend/scw_secret_wrapper"
+	"go.uber.org/mock/gomock"
 )
 
 func TestInitalisation(t *testing.T) {
 
 	t.Run("working", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
 		privKey, err := generatePrivKey(t)
 		if err != nil {
 			t.Errorf("Unexpected error, got %v", err)
 		}
-		wrapper := scw_secret_wrapper.NewScaleWayWrapperForDev(true)
-		_, err = wrapper.SetSecret("PRIVATE_KEY", privKey)
-		if err != nil {
-			t.Errorf("Unexpected error, got %v", err)
-		}
+		wrapper := scw_secret_wrapper.NewMockIScaleWayWrapper(ctrl)
+		wrapper.EXPECT().ListSecrets("PRIVATE_KEY").Return(scw_secret_wrapper.SecretHolder{Secrets: []*scw_secret_wrapper.Secret{{ID: "test", Name: "test"}}}, nil)
+		wrapper.EXPECT().ListSecretVersions(gomock.Any()).Return(scw_secret_wrapper.SecretVersionHolder{SecretVersions: []scw_secret_wrapper.SecretVersion{{Revision: 1, IsLatest: true}}}, nil)
+		wrapper.EXPECT().GetSecretData(gomock.Any(), gomock.Any()).Return(privKey, nil)
 		manager := NewSignatureManager(wrapper)
 		err = manager.InitSignatureManager()
 		if err != nil {
@@ -32,17 +33,15 @@ func TestInitalisation(t *testing.T) {
 
 	})
 	t.Run("signing hash", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
 		privKey, err := generatePrivKey(t)
 		if err != nil {
 			t.Errorf("Unexpected error, got %v", err)
 		}
-		wrapper := scw_secret_wrapper.NewScaleWayWrapperForDev(true)
-		secret, err := wrapper.SetSecret("PRIVATE_KEY", privKey)
-		t.Log(secret.ID)
-
-		if err != nil {
-			t.Errorf("Unexpected error, got %v", err)
-		}
+		wrapper := scw_secret_wrapper.NewMockIScaleWayWrapper(ctrl)
+		wrapper.EXPECT().ListSecrets("PRIVATE_KEY").Return(scw_secret_wrapper.SecretHolder{Secrets: []*scw_secret_wrapper.Secret{{ID: "test", Name: "test"}}}, nil)
+		wrapper.EXPECT().ListSecretVersions(gomock.Any()).Return(scw_secret_wrapper.SecretVersionHolder{SecretVersions: []scw_secret_wrapper.SecretVersion{{Revision: 1, IsLatest: true}}}, nil)
+		wrapper.EXPECT().GetSecretData(gomock.Any(), gomock.Any()).Return(privKey, nil)
 		signatureManager := NewSignatureManager(wrapper)
 		err = signatureManager.InitSignatureManager()
 		if err != nil {
